0a8281bba275ecd3d51267345f8a811973eff9f7
afc163 <afc163@gmail.com>
2017-08-30 20:26:11 +0800

    fix dist file

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const path = require('path');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.config.js');
@@ -7,8 +8,14 @@ common.entry = {
   'color3d': "./index.js",
 };
 
+common.plugins = [];
+
 module.exports = merge(common, {
   devtool: 'cheap-module-source-map',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       compress: {
@@ -18,8 +25,8 @@ module.exports = merge(common, {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
-  ]
-})
+        'NODE_ENV': JSON.stringify('production'),
+      },
+    }),
+  ],
+});
